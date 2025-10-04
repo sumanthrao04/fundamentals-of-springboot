a069276611388a50b8a54b1abad622ef491ac1c1
sumanthrao04 <sumanthrao75@gmail.com>
2025-10-04 14:34:30 +0530

    Setting Up Project

---
+++ src/main/java/com/sumanth/fundamentals_of_springboot/FundamentalsOfSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.sumanth.fundamentals_of_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsOfSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentalsOfSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumanth/fundamentals_of_springboot/FundamentalsOfSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumanth.fundamentals_of_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalsOfSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
